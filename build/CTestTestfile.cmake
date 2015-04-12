ef5b828b39bbeec0880dbd432efb7bfede71c07f
Josh Morse <jbmorse@wpi.edu>
2015-04-12 17:25:28 -0400

    obstacle expansion for the occupancy grid implemented (and somewhat tested)

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -5,6 +5,7 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 SUBDIRS(gtest)
+SUBDIRS(Lab_4)
 SUBDIRS(jmorse_lab2)
 SUBDIRS(jbmorse_lab3)
 SUBDIRS(lab3)
---
+++ build/Lab_4/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jbmorse/catkin_ws/src/Lab_4
+# Build directory: /home/jbmorse/catkin_ws/build/Lab_4
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
